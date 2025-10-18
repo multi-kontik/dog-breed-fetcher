282d9723dfd34d8d074f4d4dd4a24be45e557bf2
multi-kontik <157947756+multi-kontik@users.noreply.github.com>
2025-10-17 22:51:42 -0400

    TASK1: DogApiBreedFetcher.java

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,12 +24,31 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        String url = "https://dog.ceo/api/breed/" + breed.toLowerCase() + "/list";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new BreedNotFoundException("Failed to fetch breed info: \" + response");
+            }
+            String jsonData = response.body().toString();
+            JSONObject json = new JSONObject(jsonData);
+
+            if (!json.getString("status").equals("success")) {
+                throw new BreedNotFoundException("Breed not found: " + breed);
+            }
+
+            JSONArray subBreeds = json.getJSONArray("message");
+            List<String> result = new ArrayList<>();
+            for (int i = 0; i < subBreeds.length(); i++) {
+                result.add(subBreeds.getString(i));
+            }
+
+            return result;
+        }  catch (IOException e) {
+            throw new BreedNotFoundException("API call failed: " + e.getMessage());
+        }
     }
 }
